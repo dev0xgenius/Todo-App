8855f3481509a7499158b5af94a1dad17b8bb97b
0xgenius <dev0xgenius@gmail.com>
2025-01-03 15:40:02 +0100

    Made App Component stateless by using function components instead of class components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
 import TodoDashboard from './components/TodoDashboard';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="app-image">
+export default function App () {
+  return (
+    <>
+      <div className="app-image">
+      </div>
+      <main className="ui main">
+        <div className="header">
+          <h1>T O D O</h1>
+          <button id="themeSwitch">
+            <img src="/images/icon-sun.svg" alt="" className="theme-mode" />
+          </button>
         </div>
-        <main className="ui main">
-          <div className="header">
-            <h1>T O D O</h1>
-            <button id="themeSwitch">
-              <img src="/images/icon-sun.svg" alt="" className="theme-mode" />
-            </button>
-          </div>
-          <TodoDashboard />
-        </main>
-      </>
-    );
-  }
+        <TodoDashboard />
+      </main>
+    </>
+  );
 }
-
-export default App
