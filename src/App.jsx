2e4a270a7f68dadaae1fd1fbbfa5d43725f5d5fd
0xgenius <dev0xgenius@gmail.com>
2025-01-11 16:58:05 +0100

    Stripped App component by extracting header into a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,13 @@
-import React from 'react';
-import useThemeSwitch from '../utils/hooks/useThemeSwitch.js';
-import { useState } from "react";
-import Dashboard from './components/Dashboard/Dashboard.jsx';
+import Dashboard from './components/Dashboard/Dashboard';
+import Header from './components/Header/Header'
 import "./scss/index.scss";
 
 export default function App () {
-  const [theme, setTheme] = useState({
-    mode: "dark",
-    indicatorImg: "/images/icon-sun.svg",
-  });
-  
-  const toggleTheme = (evt) => {
-    const imgSrc = (theme.mode == "dark") ? 
-      "/images/icon-moon.svg" : "/images/icon-sun.svg";
-      
-    setTheme(currentTheme => (
-      {...currentTheme, 
-        mode: (currentTheme.mode == "light") ? "dark" : "light",
-        indicatorImg: imgSrc,
-      }
-    ));
-  };
-  
-  useThemeSwitch(theme);
-  
   return (
     <>
       <div className="background-image"></div>
       <main className="ui main">
-        <div className="header">
-          <h1>T O D O</h1>
-          <button onClick={toggleTheme}>
-            <img src={theme.indicatorImg} alt="" className="themeIndicator"/>
-          </button>
-        </div>
+        <Header />
         <Dashboard />
       </main>
     </>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import useThemeSwitch from '../../../utils/hooks/useThemeSwitch.js';
+
+export default function Header() {
+  const [theme, setTheme] = useState({
+    mode: "dark",
+    indicatorImg: "/images/icon-sun.svg",
+  });
+  
+  const imgSource = {
+    "dark" : "/images/icon-sun.svg",
+    "light" : "/images/icon-moon.svg"
+  };
+  
+  const switchThemeMode = () => (theme.mode == "light") ? "dark" : "light";
+  
+  const switchTheme = (evt) => {
+    const switchedMode = switchThemeMode(theme.mode);
+    setTheme(theme => (
+      {
+        mode: switchedMode,
+        indicatorImg: imgSource[switchedMode],
+      }
+    ));
+  };
+  
+  useThemeSwitch(theme);
+  
+  return (
+    <div className="header">
+      <h1>T O D O</h1>
+      <button onClick={switchTheme}>
+        <img src={theme.indicatorImg} alt="" className="themeIndicator"/>
+      </button>
+    </div>
+  )
+};
